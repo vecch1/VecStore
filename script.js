6c4295d9b7d9f0718ee83491938d918b64898d7f
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2024-12-27 14:11:44 -0300

    filtros

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-// carrinho
-const addCarrinho = document.querySelector(".comprar btn")
+document.getElementById('filter').addEventListener('change', function() {
+    const filtro = this.value;
+    const produtos = document.querySelectorAll('.main-produtos');
+    const relogio = document.querySelectorAll('.produtos-relogio');
+    const corrente = document.querySelectorAll('.produtos-corrente');
+    const camiseta = document.querySelectorAll('.produtos-camiseta');
+    const bone = document.querySelectorAll('.produtos-bone');
+    const tenis = document.querySelectorAll('.produtos-tenis');
 
-addCarrinho.addEventListener("click", function(){
-    
-})
+});
