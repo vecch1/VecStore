9da1cebef3fe639c125eef8cefcbc98e209dbccf
Lucas Vecchi Barcelos <lucasvecchibarcelos@gmail.com>
2024-12-28 15:40:46 -0300

    carrino-react

--- script.js
+++ script.js
@@ -2,23 +2,27 @@ document.getElementById('options').addEventListener('change', function() {
     const filtro = this.value;
     const produtosTodos = document.querySelectorAll('.main-produtos');
 
-    // Esconder todos os produtos
+    
     produtosTodos.forEach(produto => {
-        produto.style.display = 'none'; // Esconder todos os contêineres de produtos
+        produto.style.display = 'none'; 
     });
 
-    // Mostrar produtos com base no filtro selecionado
     if (filtro === 'all') {
         produtosTodos.forEach(produto => {
-            produto.style.display = 'flex'; // Mostrar todos os produtos
-            produto.style.flexWrap = 'wrap'; // Permitir que os itens se movam para a próxima linha
+            produto.style.display = 'flex'; 
+            produto.style.flexWrap = 'wrap'; 
         });
     } else {
-        // Mostrar apenas os produtos filtrados
         const produtosFiltrados = document.querySelectorAll(`.produtos-${filtro}`);
         produtosFiltrados.forEach(produto => {
-            produto.parentElement.style.display = 'flex'; // Mostrar apenas o contêiner do produto filtrado
-            produto.parentElement.style.flexWrap = 'wrap'; // Permitir que os itens se movam para a próxima linha
+            produto.parentElement.style.display = 'flex'; 
+            produto.parentElement.style.flexWrap = 'wrap'; 
         });
     }
 });
+
+
+// carrinho
+document.querySelectorAll('.comprar-btn').addEventListener('click' , function(){
+    
+})
